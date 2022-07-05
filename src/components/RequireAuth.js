0b3630f196f92076a1daab3b619a71015e9a7a4c
bakarbsa <abubakar.bsa30@gmail.com>
2022-07-05 11:42:52 +0700

    fix setAuth in RequireAuth

--- src/components/RequireAuth.js
+++ src/components/RequireAuth.js
@@ -1,24 +1,32 @@
+import { useEffect } from 'react';
 import { useLocation, Navigate, Outlet } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
+import Loading from './Loading';
 
 function RequireAuth({ allowedRoles }) {
   const { auth, setAuth } = useAuth();
   const location = useLocation();
 
+  useEffect(() => {
+    if (!auth.accessToken) {
+      setAuth({
+        accessToken: sessionStorage.getItem('accessToken'),
+        user: sessionStorage.getItem('user'),
+        roles: [sessionStorage.getItem('roles')],
+      });
+    }
+  }, [auth.accessToken]);
+
   return (
-    auth.accessToken !== undefined
+    auth.accessToken
       ? auth?.roles?.find((role) => allowedRoles?.includes(role))
         ? <Outlet />
         : auth?.user
           ? <Navigate to="/unauthorized" state={{ from: location }} replace />
           : <Navigate to="/login" state={{ from: location }} replace />
-      : sessionStorage.getItem('accessToken') === undefined
+      : !sessionStorage.getItem('accessToken')
         ? <Navigate to="/login" state={{ from: location }} replace />
-        : setAuth({
-          accessToken: sessionStorage.getItem('accessToken'),
-          user: sessionStorage.getItem('user'),
-          roles: [sessionStorage.getItem('roles')],
-        })
+        : <Loading />
   );
 }
 
