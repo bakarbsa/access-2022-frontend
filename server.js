9e5e45a1bf27d400941eee7175a55e1b4cbf497e
bakarbsa <abubakar.bsa30@gmail.com>
2022-08-08 11:54:37 +0800

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //server.js
-const express = require('express');
+/*const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
 const https = require('https');
@@ -30,3 +30,21 @@ app.get('/*', function (req, res) {
 });
 app.listen(port);
 https.createServer(options, app).listen(8080);
+*/
+//server.js
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 80;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
